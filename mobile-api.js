a07862bdb2921c7795716dd94049ad6147ac0858
ztashneem <z.tashneem12@gmail.com>
2022-03-02 23:15:39 +0600

    my first commit

--- mobile-api.js
+++ mobile-api.js
@@ -1,5 +1,5 @@
 // https://openapi.programming-hero.com/api/phones = full api
 // https://openapi.programming-hero.com/api/phones?search=apple
-// search can be found from this :iPhone,Samsung,Oppo,Huwaei.
 // https://openapi.programming-hero.com/api/phone/apple_iphone_13_pro_max-11089 
 // Search all the products.
+// Seach Products found :iPhone,Samsung,Oppo,Huwaei.
